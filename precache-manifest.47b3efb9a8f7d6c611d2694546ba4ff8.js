c93fef2fa2fd39eeacb3facd64c0c85ae74e1b8f
felisito1999 <60663860+felisito1999@users.noreply.github.com>
2020-08-24 10:50:36 -0400

    page

---
+++ precache-manifest.47b3efb9a8f7d6c611d2694546ba4ff8.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42ac4a55a1ec5f05b1de3558212c849a",
+    "url": "/macarfe-app/index.html"
+  },
+  {
+    "revision": "2ac3bd3e32a7fe22b624",
+    "url": "/macarfe-app/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "8cda2e5bed2e4c65ceaf",
+    "url": "/macarfe-app/static/css/main.08544cbc.chunk.css"
+  },
+  {
+    "revision": "2ac3bd3e32a7fe22b624",
+    "url": "/macarfe-app/static/js/2.7fda810f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/macarfe-app/static/js/2.7fda810f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8cda2e5bed2e4c65ceaf",
+    "url": "/macarfe-app/static/js/main.21121040.chunk.js"
+  },
+  {
+    "revision": "d2a890b44bfc7f4685e9",
+    "url": "/macarfe-app/static/js/runtime-main.4e1e8400.js"
+  },
+  {
+    "revision": "973ff746c01c8e024689a5b2e912640a",
+    "url": "/macarfe-app/static/media/AceiteCrisolGalon.973ff746.png"
+  },
+  {
+    "revision": "a47c6d075e18a4365fba540a752f2667",
+    "url": "/macarfe-app/static/media/AguaDasani20oz.a47c6d07.jpg"
+  },
+  {
+    "revision": "346b31baa9e0583029976d4608c02075",
+    "url": "/macarfe-app/static/media/ArrozDosPinos.346b31ba.jpg"
+  },
+  {
+    "revision": "07bc05c76fe06552fef11ffa103af73a",
+    "url": "/macarfe-app/static/media/AzucarBlanca.07bc05c7.jpg"
+  },
+  {
+    "revision": "13c7a623c569e77487b781fd40747639",
+    "url": "/macarfe-app/static/media/AzucarCrema.13c7a623.jfif"
+  },
+  {
+    "revision": "051c65699c7adbb15df2a7de96fc1a7a",
+    "url": "/macarfe-app/static/media/CarameloIbon.051c6569.jpg"
+  },
+  {
+    "revision": "1264142ac22bf093c9bfb2ea70e1d1ee",
+    "url": "/macarfe-app/static/media/DoñaGallina.1264142a.jpg"
+  },
+  {
+    "revision": "229155d422e21f5bd199eaeef2621deb",
+    "url": "/macarfe-app/static/media/GalletaDino.229155d4.jpg"
+  },
+  {
+    "revision": "7831806f7b35fdd679fa1b93e5cd3d7c",
+    "url": "/macarfe-app/static/media/Habichuelas.7831806f.jpeg"
+  },
+  {
+    "revision": "b7dcc06e411fe510679eeb381bfe7cbc",
+    "url": "/macarfe-app/static/media/LecheRica1Lt.b7dcc06e.jpg"
+  },
+  {
+    "revision": "d02726a15da07bff2eb1880864d4ea8a",
+    "url": "/macarfe-app/static/media/MentaHalls.d02726a1.jpg"
+  },
+  {
+    "revision": "f160e2a74e8584fd825d7990c7181d57",
+    "url": "/macarfe-app/static/media/PaletaDulceAmor.f160e2a7.jpg"
+  },
+  {
+    "revision": "f17570edac1f7fcd35ace32180f97f49",
+    "url": "/macarfe-app/static/media/SalsaFamosaSobre.f17570ed.jpg"
+  },
+  {
+    "revision": "81c8c1f5e2f2990fd14bb4118481aceb",
+    "url": "/macarfe-app/static/media/ServilletaBingo.81c8c1f5.jpg"
+  },
+  {
+    "revision": "01a9a2635802a9d727736ef8ec3d5a43",
+    "url": "/macarfe-app/static/media/SpaghettiMilano.01a9a263.jpg"
+  },
+  {
+    "revision": "89fa11bb6bb1f517c4070773c0170530",
+    "url": "/macarfe-app/static/media/correos.89fa11bb.jpg"
+  },
+  {
+    "revision": "fb9866a09017b187a4cfdf1901b58a81",
+    "url": "/macarfe-app/static/media/foto_dulces.fb9866a0.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/macarfe-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "32dda3978386b01b208c23495329fffe",
+    "url": "/macarfe-app/static/media/logo_macarfe.32dda397.svg"
+  },
+  {
+    "revision": "c3b12268bb8d29ad7f91257fd58fc801",
+    "url": "/macarfe-app/static/media/truck_logo.c3b12268.svg"
+  }
+]);
